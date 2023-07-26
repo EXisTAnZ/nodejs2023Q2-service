220f7cb6db68ae2e38882d7d17fb3d2d70ea2d58
EXisTAnZ <ozim_ssspam@mail.ru>
2023-07-26 21:55:12 +0300

    feat: add users model and dto

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { v4 } from 'uuid';
+
+export class User {
+  public id: string;
+  public login: string;
+  public password: string;
+  public version: number;
+  public createdAt: number;
+  public updatedAt: number;
+
+  constructor(login: string, password: string) {
+    this.id = v4();
+    this.login = login;
+    this.password = password;
+    this.version = 1;
+    this.createdAt = new Date().getTime();
+    this.updatedAt = new Date().getTime();
+  }
+
+  public update(login: string, newPassword: string) {
+    this.login = login;
+    this.password = newPassword;
+    this.version++;
+    this.updatedAt = new Date().getTime();
+  }
+}
