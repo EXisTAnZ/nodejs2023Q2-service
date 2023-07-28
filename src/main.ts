a9fb673fce396653fae0f8630a4942e4a124982b
EXisTAnZ <ozim_ssspam@mail.ru>
2023-07-28 17:56:46 +0300

    feat: install @nestjs/config for work with env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const config = new ConfigService();
+  const PORT = config.get<number>('PORT');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(4000);
+  await app.listen(PORT || 4000);
 }
 bootstrap();
