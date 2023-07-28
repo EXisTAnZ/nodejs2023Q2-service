f89cc4a8ce6d1bcd9bb6662460a250c479502782
EXisTAnZ <ozim_ssspam@mail.ru>
2023-07-28 11:41:08 +0300

    feat: add album dto and class

--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,1 +1,24 @@
-export class CreateAlbumDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  @Min(0)
+  @Max(2023)
+  @IsNotEmpty()
+  year: number;
+
+  @IsString()
+  @IsOptional()
+  artistId: string | null;
+}
