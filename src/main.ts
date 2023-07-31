a2ca26a8d26f0c55d77040adea68310f660f13b9
EXisTAnZ <ozim_ssspam@mail.ru>
2023-07-31 10:53:07 +0300

    feat: add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const config = new ConfigService();
   const PORT = config.get<number>('PORT');
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Home library')
+    .setDescription('The home library API description')
+    .setVersion('1.0')
+    .addTag('doc')
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('doc', app, doc);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT || 4000);
 }
